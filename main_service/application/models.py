a1b7b457b172918a29e3802c699105c5f8c32c2e
PKauldhar <parminder.kauldhar@qa.com>
2020-03-05 14:23:58 +0000

    update

--- main_service/application/models.py
+++ main_service/application/models.py
@@ -21,21 +21,11 @@ class Users(db.Model, UserMixin):
 class Movies(db.Model):
         id = db.Column(db.Integer, primary_key=True)
         title = db.Column(db.String(30), nullable=False, unique=True)
+        genre = db.Column(db.String(30), nullable=False)
         director = db.Column(db.String(30), nullable=False)
         rating = db.Column(db.String(1), nullable=False)
         date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
         user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-        genre = db.relationship("Genres", backref="movies", lazy=True)
 
         def __repr__(self):
             return ''.join(["User ID: ",self.user_id,"\r\n,""Title: ", self.title,"\r\n","Genre: ", self.genre,"Director:  ",self.director,"\r\n","Rating: ",self.rating])
-
-
-
-class Genres(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        genre = db.Column(db.String(30), nullable=False)
-        movieid = db.Column(db.Integer, db.ForeignKey("movies.id"), nullable=False)
-
-        def __repr__(self):
-            return ''.join(["Movies ID: ",self.movieid,"\r\n,""Genre ID: ", self.id,"\r\n","Genre: ", self.genre])
