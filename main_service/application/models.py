0dd24ab39e1cf8d5c4c84bf1c97dc40d4eadf66b
PKauldhar <parminder.kauldhar@qa.com>
2020-03-05 14:22:47 +0000

    update

--- main_service/application/models.py
+++ main_service/application/models.py
@@ -19,7 +19,7 @@ class Users(db.Model, UserMixin):
 
 
 class Movies(db.Model):
-        movieid = db.Column(db.Integer, primary_key=True)
+        id = db.Column(db.Integer, primary_key=True)
         title = db.Column(db.String(30), nullable=False, unique=True)
         director = db.Column(db.String(30), nullable=False)
         rating = db.Column(db.String(1), nullable=False)
