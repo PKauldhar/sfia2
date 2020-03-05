4fe3fda285dce0a19f6782efec4d82f64fd64762
PKauldhar <parminder.kauldhar@qa.com>
2020-03-05 17:03:18 +0000

    update

--- random_genre/application/models.py
+++ random_genre/application/models.py
@@ -4,18 +4,6 @@ from datetime import datetime
 
 #Test change for jenkins
 
-class Users(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(500), nullable=False, unique=True)
-    password = db.Column(db.String(500), nullable=False)
-    movies = db.relationship("Movies", backref="author", lazy=True)
-
-    def __repr__(self):
-        return ''.join(["UserID: ", str(self.id), "\r\n", "Email: ", self.email])
-    @login_manager.user_loader
-    def load_user(id):
-        return Users.query.get(int(id))
-
 
 class Movies(db.Model):
         id = db.Column(db.Integer, primary_key=True)
