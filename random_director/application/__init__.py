8ce1cf44cbbd9af885a39d93bc5471f20f05b7ab
PKauldhar <parminder.kauldhar@qa.com>
2020-03-06 12:08:37 +0000

    update

--- random_director/application/__init__.py
+++ random_director/application/__init__.py
@@ -1,11 +1,14 @@
+from flask_login import LoginManager
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
-
+from flask_bcrypt import Bcrypt
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']= os.getenv('DATABASE_URI')
 db = SQLAlchemy(app)
 from os import getenv
-#this is all done in the yaml file now with the environment variables to see
 app.config['SECRET_KEY']= getenv('SECRET_KEY')
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 from application import routes
