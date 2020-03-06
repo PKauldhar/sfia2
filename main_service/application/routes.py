4f353bede7541ce923bf0f208249da3b61d0b87c
PKauldhar <parminder.kauldhar@qa.com>
2020-03-06 11:37:29 +0000

    update

--- main_service/application/routes.py
+++ main_service/application/routes.py
@@ -6,22 +6,6 @@ from application.forms import addMovie, RegistrationForm, LoginForm, EmailChange
 import requests
 
 
-@app.route('/randomMovie')
-def randomMovie():
- rg=requests.get('http://projects_random_genre_1:5000/randomGenre')
- random_genre=rg.text
- movies_genre=Movies.query.filter_by(genre=random_genre).all()
-
- return render_template('randomMovie.html', title='randomMovie', random=random_genre)
-
-@app.route('/randomDirector')
-def randomDirector():
- rd=requests.get('http://projects_random_director_1:5000/randomDirector')
- random_director=rd.text
- movies_director=Movies.query.filter_by(director=random_director).all()
-
- return render_template('randomMovie.html', title='randomMovie', randomdirector=random_director)
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
@@ -111,6 +95,23 @@ def deleteMovie():
     else:
         print(form.errors)
     return render_template('deleteMovie.html', title='Delete', form=form)
+
+
+@app.route('/randomMovie')
+def randomMovie():
+ rg=requests.get('http://projects_random_genre_1:5000/randomGenre')
+ random_genre=rg.text
+ movies_genre=Movies.query.filter_by(genre=random_genre).all()
+
+ return render_template('randomMovie.html', title='randomMovie', random=random_genre)
+
+@app.route('/randomDirector')
+def randomDirector():
+ rd=requests.get('http://projects_random_director_1:5000/randomDirector')
+ random_director=rd.text
+ movies_director=Movies.query.filter_by(director=random_director).all()
+
+ return render_template('randomMovie.html', title='randomMovie', randDir=random_director)
 
 
 @app.route("/account", methods=['GET', 'POST'])
