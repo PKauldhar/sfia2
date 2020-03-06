70a14ecdcc8c4e2fb667a8c38ea96d2da8ee577f
PKauldhar <parminder.kauldhar@qa.com>
2020-03-06 10:28:41 +0000

    randomdirector

--- random_director/application/routes.py
+++ random_director/application/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, request,Response
 from application import app, db
 from application.models import Movies, Users
-#from application.forms import favGenre
+
 import requests
 import random
 
@@ -18,6 +18,6 @@ def get_randomDirector():
        director_list = list(dict.fromkeys(director_list)) #removes duplicates
 
     random_director = random.choice(director_list)
-    #print(random_genre)
+
     return random_director
 #	return "hi"
