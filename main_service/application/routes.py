c5da260f65513e6f75dfd056954eac859572cf86
PKauldhar <parminder.kauldhar@qa.com>
2020-03-06 10:55:46 +0000

    randomdirector

--- main_service/application/routes.py
+++ main_service/application/routes.py
@@ -10,7 +10,7 @@ import requests
 def randomMovie():
  rg=requests.get('http://projects_random_genre_1:5000/randomGenre')
  random_genre=rg.text
- movies_genre=Movies.query.filter_by(genre=random_genre).all()
+ movies_genre=Movies.query.filter_by(user_id=current_user.id, genre=random_genre).all()
 
  return render_template('randomMovie.html', title='randomMovie', random=random_genre)
 
@@ -18,7 +18,7 @@ def randomMovie():
 def randomDirector():
  rd=requests.get('http://projects_random_director_1:5000/randomDirector')
  random_director=rd.text
- movies_director=Movies.query.filter_by(director=random_director).all()
+ movies_director=Movies.query.filter_by(user_id=current_user.id, director=random_director).all()
 
  return render_template('randomMovie.html', title='randomMovie', randomdirector=random_director)
 
