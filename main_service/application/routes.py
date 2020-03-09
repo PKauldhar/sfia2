4c7ac84efa2cf774305e3ea1a75688c3927668d6
PKauldhar <parminder.kauldhar@qa.com>
2020-03-09 12:26:09 +0000

    9/3/2020

--- main_service/application/routes.py
+++ main_service/application/routes.py
@@ -48,10 +48,9 @@ def home():
 @app.route('/randomMovie', methods=['GET', 'POST'])
 @login_required
 def randomMovie():
-   # currentuser=int(current_user.id)
-    random_movies=requests.post('http://projects_random_master_1:5000/randomMaster',current_user.id)
-
-
+    currentuser=str(current_user.id)
+    random_movies=requests.post('http://projects_random_master_1:5000/randomMaster',currentuser)
+    
     return render_template('randomMovie.html', title='randomMovie', randommovies=random_movies)
 
 
