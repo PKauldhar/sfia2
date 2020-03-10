98f7c21b53f1a58f54e5c451a9059333896103fd
PKauldhar <parminder.kauldhar@qa.com>
2020-03-10 14:29:31 +0000

    csv data

--- main_service/create.py
+++ main_service/create.py
@@ -1,6 +1,15 @@
 from application import db
 from application.models import Movies
+import pandas as pd
 
 db.drop_all()
 db.create_all()
-#pc
+
+
+data = pd.read_csv('./movies.csv')
+data.columns= [0,1,2,3]
+
+for index,row in data.iterrows():
+	Movie = Movies(title=row[0], genre =row[2], director=row[3], rating=row[1])
+	db.session.add(Movie)
+db.session.commit()
